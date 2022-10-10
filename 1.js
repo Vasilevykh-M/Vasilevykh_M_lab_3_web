39ab55301b378c0ca5eb1116c3b9d31222510d41
Vasilevykh-M <vasilevyh00@gmail.com>
2022-10-10 11:44:36 +0500

    fix Placeholder and view en and ru form
    fix Placeholder and view en and ru form

--- 1.js
+++ 1.js
@@ -1,11 +1,23 @@
 function addInputField()
 {
     var data = document.getElementById("input_field_form").value;
-    parent = document.getElementById("Heap");
+    parent_RU = document.getElementById("Heap_RU");
+    parent_EN = document.getElementById("Heap_EN");
     let form_to_add = document.createElement('input');
     form_to_add.classList.add('input_field');
     form_to_add.placeholder = data;
     let br = document.createElement('br');
-    parent.appendChild(form_to_add);
-    parent.appendChild(br);
+
+    
+    parent_RU.appendChild(form_to_add);
+    parent_RU.appendChild(br);
+
+
+    let form_to_add_ = document.createElement('input');
+    form_to_add_.classList.add('input_field');
+    form_to_add_.placeholder = data;
+    let br_ = document.createElement('br');
+
+    parent_EN.appendChild(form_to_add_);
+    parent_EN.appendChild(br_);
 }
